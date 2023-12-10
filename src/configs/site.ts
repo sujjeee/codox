745e0e01d613a31b80ec98f0dd43e75f49ce0078
Suraj <sujjeee@outlook.com>
2023-12-10 21:28:12 +0530

    Updated site config

--- src/configs/site.ts
+++ src/configs/site.ts
@@ -1,14 +1,14 @@
-export type SiteConfig = typeof siteConfig
+export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "Web Development Stacks",
   description:
     "A starter template for modern web development with nextjs 13 (app route), tailwindcss, typescript, trpc, clerk auth, and prisma.",
-  url: "https://codox.codebustar.com/",
-  ogImage: "https://codox.codebustar.com/opengraph-image.png",
+  url: "https://codox.vercel.app/",
+  ogImage: "https://codox.vercel.app/opengraph-image.png",
   author: {
     name: "sujjeee",
     twitter: "https://twitter.com/sujjeeee",
     github: "https://github.com/sujjeee",
-  }
-}
+  },
+};
